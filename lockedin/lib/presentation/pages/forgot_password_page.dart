46216d5e3abc287c648df8ea7265cbc23070c815
bassel-hammad <bassel.shahat03@eng-st.cu.edu.eg>
2025-03-03 17:02:44 +0200

    forgot_password_page Done

---
+++ lockedin/lib/presentation/pages/forgot_password_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Forgot password',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email or Phone',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 15),
+            Text(
+              "We'll send a verification code to this email or phone number if it matches an existing LinkedIn account.",
+              style: TextStyle(fontSize: 14,
+              color: Colors.black),
+            ),
+            SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  backgroundColor: const Color.fromARGB(255, 1, 95, 171),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+                child: Text(
+                  'Next',
+                  style: TextStyle(fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
