34bf79e89ad1e1f02ad9dfa2f049b65b752843dc
ranayasser-lab <115491760+ranayasser-lab@users.noreply.github.com>
2025-03-03 20:56:38 +0200

    sign up

--- lockedin/lib/main.dart
+++ lockedin/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lockedin/presentation/pages/main_page.dart';
+import 'package:lockedin/presentation/pages/sign_up_view.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp())); // Enable Riverpod
