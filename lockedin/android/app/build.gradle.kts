fa5e3491f5483c16677cfd204323e6fda0e6b92c
ranayasser-lab <115491760+ranayasser-lab@users.noreply.github.com>
2025-03-11 17:43:48 +0200

    sign up and veriication email

--- lockedin/android/app/build.gradle.kts
+++ lockedin/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.lockedin"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "26.3.11579264"
+    ndkVersion = "28.0.13004108"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- lockedin/lib/main.dart
+++ lockedin/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'LinkedIn Clone',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: SignUpView(),
+      home: MainPage(),
     );
   }
 }
