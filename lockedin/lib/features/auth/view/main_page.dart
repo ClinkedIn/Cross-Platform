0d456652e648465596750c8c2b779b5bbe6b2028
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2025-03-12 23:17:45 +0200

    fixes

--- lockedin/lib/features/auth/view/main_page.dart
+++ lockedin/lib/features/auth/view/main_page.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lockedin/shared/widgets/side_bar.dart';
-//import 'package:lockedin/presentation/pages/profile_page.dart';
 import 'package:lockedin/features/profile/viewmodel/profile_viewmodel.dart';
 import 'package:lockedin/shared/widgets/upper_navbar.dart';
-import '../../home_page/view/home_page.dart';
-import '../../networks/view/network_page.dart';
-import '../../post/view/post_page.dart';
-import '../../notifications/view/notifications_page.dart';
-import '../../jobs/view/jobs_page.dart';
-import '../../../shared/widgets/bottom_navbar.dart';
+import 'package:lockedin/features/home_page/view/home_page.dart';
+import 'package:lockedin/features/networks/view/network_page.dart';
+import 'package:lockedin/features/post/view/post_page.dart';
+import 'package:lockedin/features/notifications/view/notifications_page.dart';
+import 'package:lockedin/features/jobs/view/jobs_page.dart';
+import 'package:lockedin/shared/widgets/bottom_navbar.dart';
 
 class MainPage extends ConsumerWidget {
   MainPage({Key? key}) : super(key: key);
 
-  final GlobalKey<ScaffoldState> _scaffoldKey =
-      GlobalKey<ScaffoldState>(); // 🔥 Add this key
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -31,7 +29,7 @@ class MainPage extends ConsumerWidget {
     ];
 
     return Scaffold(
-      key: _scaffoldKey, // 🔥 Attach the key here
+      key: _scaffoldKey,
       appBar: UpperNavbar(
         leftIcon: currentUserAsync.when(
           data:
@@ -55,8 +53,7 @@ class MainPage extends ConsumerWidget {
         ),
 
         leftOnPress: () {
-          _scaffoldKey.currentState
-              ?.openDrawer(); // 🔥 Use key to open drawer safely
+          _scaffoldKey.currentState?.openDrawer();
         },
       ),
       drawer: SidebarDrawer(),
