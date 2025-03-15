ddcfa39e13320fb41911cc16bb392f8b2210fd3d
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2025-03-15 17:06:52 +0200

    Added essential core services and utilities for better project structure

---
+++ lockedin/lib/core/utils/constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  static const String defaultProfileImage =
+      'assets/images/default_profile_photo.png';
+  static const String defaultCoverPhoto =
+      'assets/images/default_cover_photo.jpeg';
+  static const String baseUrl = 'https://your-api.com/api';
+
+  static const String loginEndpoint = '/user/login';
+  static const String registerEndpoint = '/user/';
+  static const String createUserProfileEndpoint = '/user/profile';
+  static const String logoutEndpoint = '/user/logout';
+}
