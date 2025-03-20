6b5cd2bee58b5cd869876b98099a84c62371f49b
ranayasser-lab <115491760+ranayasser-lab@users.noreply.github.com>
2025-03-20 15:55:58 +0200

    profile components

---
+++ lockedin/lib/features/profile/model/profile_item_model.dart
@@ -0,0 +1,13 @@
+class ProfileItemModel {
+  final String title;
+  final String subtitle;
+  final String duration;
+  final String logoUrl;
+
+  ProfileItemModel({
+    required this.title,
+    required this.subtitle,
+    required this.duration,
+    required this.logoUrl,
+  });
+}
