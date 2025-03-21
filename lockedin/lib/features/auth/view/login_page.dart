ef7b6012a771ea9eabf34bf8622657bd881ce427
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2025-03-21 15:27:07 +0200

    login viewmodel and services done

--- lockedin/lib/features/auth/view/login_page.dart
+++ lockedin/lib/features/auth/view/login_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-//import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lockedin/features/auth/view/main_page.dart';
+import 'package:lockedin/features/auth/view/sign_up_view.dart';
+import 'package:lockedin/features/auth/viewmodel/login_in_viewmodel.dart';
+import 'package:lockedin/main.dart';
 import 'package:lockedin/shared/theme/colors.dart';
 import 'package:lockedin/shared/theme/styled_buttons.dart';
 import 'package:lockedin/shared/theme/text_styles.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final isDarkMode = theme.brightness == Brightness.dark;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final loginState = ref.watch(loginViewModelProvider);
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -34,14 +37,24 @@ class LoginPage extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 42),
-            Text("Sign in", style: theme.textTheme.headlineLarge),
+            Text("Sign in", style: Theme.of(context).textTheme.headlineLarge),
             const SizedBox(height: 8),
             Row(
               children: [
                 Text("or ", style: AppTextStyles.bodyText2),
                 TextButton(
-                  onPressed: () {},
-                  child: Text("Join lockedIn", style: AppTextStyles.buttonText.copyWith(color: AppColors.primary)),
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpView()),
+                    );
+                  },
+                  child: Text(
+                    "Join LockedIn",
+                    style: AppTextStyles.buttonText.copyWith(
+                      color: AppColors.primary,
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -50,9 +63,9 @@ class LoginPage extends StatelessWidget {
               controller: _emailController,
               decoration: InputDecoration(
                 labelText: "Email or Phone",
-                filled: true,
-                fillColor: isDarkMode ? AppColors.black : AppColors.white,
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
             ),
             const SizedBox(height: 12),
@@ -61,54 +74,60 @@ class LoginPage extends StatelessWidget {
               obscureText: true,
               decoration: InputDecoration(
                 labelText: "Password",
-                filled: true,
-                fillColor: isDarkMode ? AppColors.black : AppColors.white,
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
             ),
             const SizedBox(height: 24),
-            TextButton(
-              onPressed: () {},
-              child: Text("Forgot password?", style: AppTextStyles.buttonText.copyWith(color: AppColors.primary)),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              style: AppButtonStyles.elevatedButton,
-              onPressed: () {},
-              child: const Text("Sign in"),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: const [
-                Expanded(child: Divider()),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8),
-                  child: Text("or"),
-                ),
-                Expanded(child: Divider()),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Column(
-              children: [
-                AppButtonStyles.socialLoginButton(
-                  text: "Sign in with Apple",
-                  icon: Icons.apple,
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 12),
-                AppButtonStyles.socialLoginButton(
-                  text: "Sign in with Google",
-                  icon: Icons.g_mobiledata,
-                  onPressed: () {},
-                ),
-              ],
+            loginState.when(
+              data:
+                  (_) => ElevatedButton(
+                    style: AppButtonStyles.elevatedButton,
+                    onPressed: () async {
+                      await ref
+                          .read(loginViewModelProvider.notifier)
+                          .login(
+                            _emailController.text,
+                            _passwordController.text,
+                          );
+
+                      // If successful, navigate to Home Page
+                      if (ref.read(loginViewModelProvider).hasValue) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => MainPage()),
+                        );
+                      }
+                    },
+                    child: const Text("Sign in"),
+                  ),
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error:
+                  (error, _) => Column(
+                    children: [
+                      Text(
+                        "Error: $error",
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await ref
+                              .read(loginViewModelProvider.notifier)
+                              .login(
+                                _emailController.text,
+                                _passwordController.text,
+                              );
+                        },
+                        child: const Text("Retry"),
+                      ),
+                    ],
+                  ),
             ),
           ],
         ),
       ),
     );
   }
-
- 
 }
