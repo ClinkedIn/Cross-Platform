6eb3df3254c4fc8a608b36c1becaf02d8812c52a
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2025-04-10 00:14:11 +0200

    auth services and cookie management

--- lockedin/lib/core/utils/constants.dart
+++ lockedin/lib/core/utils/constants.dart
@@ -3,9 +3,10 @@ class Constants {
       'assets/images/default_profile_photo.png';
   static const String defaultCoverPhoto =
       'assets/images/default_cover_photo.jpeg';
-  static const String baseUrl = 'https://your-api.com/api';
+  static const String baseUrl = "http://10.0.2.2:3000";
 
   static const String loginEndpoint = '/user/login';
+  static const String getUserDataEndpoint = "/user/me";
   static const String registerEndpoint = '/user/';
   static const String createUserProfileEndpoint = '/user/profile';
   static const String logoutEndpoint = '/user/logout';
--- lockedin/lib/main.dart
+++ lockedin/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lockedin/features/home_page/view/home_page.dart';
-import 'package:lockedin/features/jobs/view/jobs_page.dart';
+import 'package:lockedin/features/auth/view/main_page.dart';
 import 'package:sizer/sizer.dart';
-import 'package:lockedin/features/auth/view/login_page.dart';
 import 'package:lockedin/shared/theme/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
@@ -32,7 +30,7 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: 'LockedIn',
       theme: theme,
-      home: MainPage(), // Change this to LoginPage() to show the login page
+      home: MainPage(),
     );
   }
 }
