04adac84804aa2c8f9b4f8475cb0a0ba4d28b477
MohamedNabil111 <mohameddnabill04@gmail.com>
2025-04-12 22:00:53 +0200

    began integrating notifications with backend

--- lockedin/lib/core/utils/constants.dart
+++ lockedin/lib/core/utils/constants.dart
@@ -14,5 +14,12 @@ class Constants {
   static const String savePostEndpoint = '/posts/%s/save'; // %s will be replaced with the postId
   static const String togglelikePostEndpoint = '/posts/%s/like'; // %s will be replaced with the postId
   //static const String unlikePostEndpoint = '/posts/%s/unlike'; // %s will be replaced with the postId
-  
+  static const String getNotificationsEndpoint = '/notifications';
+  static const String markNotificationAsReadEndpoint = '/notifications/mark-read/%s'; // %s will be replaced with the notificationId
+  static const String markNotificationAsUnreadEndpoint = '/notifications/mark-unread/%s'; // %s will be replaced with the notificationId
+  static const String getNotificationsUnreadCountEndpoint = '/notifications/unread-count';
+  static const String pauseNotificationsEndpoint = '/notifications/pause-notifications';
+  static const String resumeNotificationsEndpoint = '/notifications/resume-notifications';
+  static const String restoreNotificationsEndpoint = '/notifications/restore-notification/%s'; // %s will be replaced with the notificationId
+  static const String deleteNotificationEndpoint = '/notifications/%s'; // %s will be replaced with the notificationId
 }
