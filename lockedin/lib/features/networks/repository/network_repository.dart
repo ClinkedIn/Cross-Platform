e2070129e2ec8bb1200fdb075097e5aaab63dbe9
Mxdness <abdelrahman.amsnour04@eng-st.cu.edu.eg>
2025-04-28 23:42:31 +0300

    - Modified Connect Card to have states - Integerated send connect request - Integerated fetching suggestions for network page - Navigate to other user profiles from network page

--- lockedin/lib/features/networks/repository/network_repository.dart
+++ lockedin/lib/features/networks/repository/network_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
+import 'package:lockedin/features/networks/model/suggestion_model.dart';
 import 'package:lockedin/core/services/request_services.dart';
 import '../model/request_list_model.dart';
 
@@ -44,6 +45,52 @@ class RequestListService {
     } catch (e) {
       print('Error updating request status: $e');
       throw Exception('Error updating request status: $e');
+    }
+  }
+}
+
+class SuggestionService {
+  static const String baseUrl = 'localhost:3000';
+  final http.Client _httpClient;
+
+  SuggestionService({http.Client? httpClient})
+    : _httpClient = httpClient ?? http.Client();
+
+  Future<SuggestionList> getSuggestions({int limit = 10}) async {
+    // API call to get connection suggestions
+    try {
+      final response = await RequestService.get(
+        'user/connections/related-users',
+      );
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      if (response.statusCode == 200) {
+        return suggestionListFromJson(response.body);
+      } else {
+        throw Exception('Failed to load requests: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching requests: $e');
+    }
+  }
+
+  Future<void> sendConnectionRequest(String userId) async {
+    try {
+      final body = {"targetUserId": userId};
+      // Using PATCH with the specified JSON format
+      final response = await RequestService.post(
+        "/user/connections/request/$userId",
+        body: body,
+      );
+      if (response.statusCode != 200) {
+        throw Exception(
+          'Failed to send connect request status: ${response.statusCode}',
+        );
+      }
+    } catch (e) {
+      print('Error sending connect request status: $e');
+      throw Exception('Error sending connect request status: $e');
     }
   }
 }
