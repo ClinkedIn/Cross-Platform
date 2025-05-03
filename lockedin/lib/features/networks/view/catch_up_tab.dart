dc99729e8d1a0dbd2d0250bd84a84a96defc80b8
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2025-05-04 02:22:33 +0300

    fixes

--- lockedin/lib/features/networks/view/catch_up_tab.dart
+++ lockedin/lib/features/networks/view/catch_up_tab.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:lockedin/features/networks/view/message_request_list.dart';
 import 'package:lockedin/features/networks/viewmodel/message_view_model.dart';
 import 'package:provider/provider.dart';
 
 class CatchUpPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => MessageRequestViewModel(),
-      child: MessageRequestListScreen(),
+      child: Scaffold(
+        appBar: AppBar(title: Text('Catch Up'), centerTitle: true),
+        body: Consumer<MessageRequestViewModel>(
+          builder: (context, viewModel, child) {
+            return Center(child: Text('No new messages yet!'));
+          },
+        ),
+      ),
     );
   }
 }
